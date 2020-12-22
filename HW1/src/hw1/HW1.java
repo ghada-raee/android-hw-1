40a6a44c9cc1a4a15dcfec3fc3b464217b73df00
ghada-raee.m1-51 <76392974+ghada-2000@users.noreply.github.com>
2020-12-22 14:11:54 +0300

    done

---
+++ HW1/src/hw1/HW1.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hw1;
+
+/**
+ *
+ * @author Ghada Raee
+ */
+public class HW1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String name = "غادة";
+        int age = 20;
+        legalage(name,age);
+        
+    }
+    public static void legalage(String name, int age){
+        if(age>=18)
+            System.out.println("مبروك يا "+name+" سوف تحصل على رخصة قيادة");
+        else
+            System.out.println("للأسف يا "+name+" لا زلت صغير للحصول على رخصة قيادة");
+    }
+    
+}
